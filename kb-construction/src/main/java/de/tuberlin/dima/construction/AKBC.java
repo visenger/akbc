42207151454ec56db0af05410c3850a85c3fe47b
visenger <visenger@gmail.com>
2013-05-16 12:35:06 +0200

    initial module structure added;

---
+++ kb-construction/src/main/java/de/tuberlin/dima/construction/AKBC.java
@@ -0,0 +1,10 @@
+package de.tuberlin.dima.construction;
+
+/**
+ * AKBC is the main class to start a knowledge base construction process.
+ */
+public class AKBC {
+    public static void main(String[] args) {
+        System.out.println("...cold start knowledge base population...");
+    }
+}
---
+++ kb-construction/src/main/java/de/tuberlin/dima/coordination/Coordinator.java
@@ -0,0 +1,11 @@
+package de.tuberlin.dima.coordination;
+
+/**
+ *     Coordinator is a main class to perform a joint entity linking and
+ *     slot filling tasks for the knowledge base construction.
+ */
+public class Coordinator {
+    public static void main(String[] args) {
+        System.out.println("...start coordination..");
+    }
+}
