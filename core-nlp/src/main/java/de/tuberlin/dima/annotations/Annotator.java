b2b8510dd4928660b602ac5329ca90afe05f7a27
Priska Herger <priska@23bit.net>
2013-05-21 21:57:20 +0200

    added dummy classes to establish the package structure.

---
+++ core-nlp/src/main/java/de/tuberlin/dima/annotations/Annotator.java
@@ -0,0 +1,11 @@
+package de.tuberlin.dima.annotations;
+
+/**
+ * Date: 5/21/13
+ * Time: 9:49 PM
+ *
+ * @author Priska Herger
+ */
+public class Annotator {
+
+}
---
+++ resource-integration/src/main/java/de/tuberlin/dima/wikilinks/WikiLinker.java
@@ -0,0 +1,11 @@
+package de.tuberlin.dima.wikilinks;
+
+/**
+ * Date: 5/21/13
+ * Time: 9:52 PM
+ *
+ * @author Priska Herger
+ */
+public class WikiLinker {
+
+}
